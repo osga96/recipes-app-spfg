0079f4b09e230c1619268f3907df3a09d53f8b6e
ÓscarGarcíaSáez <osga96@gmail.com>
2022-04-21 09:54:16 +0200

    #178 - Spring MockMVC test.

--- src/test/java/com/example/learn/recipes_app/controller/IndexControllerTest.java
+++ src/test/java/com/example/learn/recipes_app/controller/IndexControllerTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.Arrays;
@@ -18,6 +20,9 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class IndexControllerTest {
 
@@ -51,6 +56,16 @@ class IndexControllerTest {
         } catch (Exception e) {
             autoCloseable = null;
         }
+    }
+
+
+
+    @Test
+    void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 
     @Test
